edea46829fb500df4cb2691d33e1729ebf50733d
Bruno Rodrigues <bruno-nascimento.rodrigues@caixa.gov.br>
2025-08-28 15:54:11 -0300

    fix(storybook): remove mockdata

--- packages/ui-components/src/components/Extract-List/Extract-List.stories.tsx
+++ packages/ui-components/src/components/Extract-List/Extract-List.stories.tsx
@@ -33,59 +33,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const mockData = [
-  {
-    date: 'Hoje',
-    data: [
-      {
-        service: 'Pix Enviado',
-        detail: 'Evelyn Almeida Souza',
-        value: 'R$ -1000,00',
-      },
-      {
-        service: 'Recebimento Pix',
-        detail: 'Ryan Melo Cardoso',
-        value: 'R$ 550,00',
-      },
-    ],
-  },
-  {
-    date: 'Qui. 14 de ago.',
-    data: [
-      {
-        service: 'Depósito',
-        detail: 'Caixa Eletrônico',
-        value: 'R$ 800,00',
-      },
-      {
-        service: 'Resgate',
-        detail: 'Fundo Fic Executivo',
-        value: 'R$ 550,00',
-      },
-      {
-        service: 'Seguro Vida',
-        detail: 'Caixa Vida e Previdência',
-        value: 'R$ -58,00',
-      },
-    ],
-  },
-  {
-    date: 'Seg. 11 de ago.',
-    data: [
-      {
-        service: 'Pagamento de Boleto',
-        detail: 'P&G Com e Serviços',
-        value: 'R$ -150,00',
-      },
-      {
-        service: 'Compra',
-        detail: 'DC Empreendimentos',
-        value: 'R$ -1.345.550,00',
-      },
-    ],
-  },
-];
-
 export const Basic: Story = {
   args: {
     data: mockData,
---
+++ packages/ui-components/src/components/Extract-List/mock-example.ts
@@ -0,0 +1,52 @@
+export const mockData = [
+  {
+    date: 'Hoje',
+    data: [
+      {
+        service: 'Pix Enviado',
+        detail: 'Evelyn Almeida Souza',
+        value: 'R$ -1000,00',
+      },
+      {
+        service: 'Recebimento Pix',
+        detail: 'Ryan Melo Cardoso',
+        value: 'R$ 550,00',
+      },
+    ],
+  },
+  {
+    date: 'Qui. 14 de ago.',
+    data: [
+      {
+        service: 'Depósito',
+        detail: 'Caixa Eletrônico',
+        value: 'R$ 800,00',
+      },
+      {
+        service: 'Resgate',
+        detail: 'Fundo Fic Executivo',
+        value: 'R$ 550,00',
+      },
+      {
+        service: 'Seguro Vida',
+        detail: 'Caixa Vida e Previdência',
+        value: 'R$ -58,00',
+      },
+    ],
+  },
+  {
+    date: 'Seg. 11 de ago.',
+    data: [
+      {
+        service: 'Pagamento de Boleto',
+        detail: 'P&G Com e Serviços',
+        value: 'R$ -150,00',
+      },
+      {
+        service: 'Compra',
+        detail: 'DC Empreendimentos',
+        value: 'R$ -1.345.550,00',
+      },
+    ],
+  },
+];
