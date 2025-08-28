da76adee9bbe73dfad1ce55297f8cdda8c6f1955
Bruno Rodrigues <bruno-nascimento.rodrigues@caixa.gov.br>
2025-08-28 15:01:06 -0300

    docs: create ExtractListProps documetation

--- packages/ui-components/src/components/Extract-List/Extract-List.model.ts
+++ packages/ui-components/src/components/Extract-List/Extract-List.model.ts
@@ -1,3 +1,10 @@
+/**
+ * Propriedades para o componente ExtractList.
+ *
+ * @property {any} [data] - Dados que serão exibidos na lista de extrato.
+ * @property {string} [testID] - Identificador utilizado para testes automatizados.
+ * @property {() => Promise<void>} [refreshAction] - Função chamada ao solicitar a atualização dos dados.
+ */
 export interface ExtractListProps {
   data: any;
 
