1047802b0fde0083072455765dcba0f2ee830c11
Bruno Rodrigues <bruno-nascimento.rodrigues@caixa.gov.br>
2025-09-01 12:00:46 -0300

    refactor(ExtractList): remove some properties from ExtractListProps

---
+++ packages/ui-components/src/components/ExtractList/ExtractList.model.ts
@@ -0,0 +1,19 @@
+/**
+ * Visual variants for the CardAlert component.
+ */
+export type ExtractListVariant = 'success' | 'neutral' | 'danger';
+
+/**
+ * Propriedades para o componente ExtractList.
+ *
+ * @property {any} [data] - Dados que serão exibidos na lista de extrato.
+ * @property {string} [testID] - Identificador utilizado para testes automatizados.
+ * @property {() => Promise<void>} [refreshAction] - Função chamada ao solicitar a atualização dos dados.
+ */
+export interface ExtractListProps {
+  data: any;
+
+  testID?: string;
+
+  refreshAction?: () => Promise<void>;
+}
