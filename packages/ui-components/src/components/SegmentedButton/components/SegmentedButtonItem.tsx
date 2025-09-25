e1e2f812677dc3e60b388bbd2b788eeec263a274
Wallisson de Matos Martins <wallisson.martins@caixa.gov.br>
2025-09-25 14:17:52 +0000

    Merged PR 20112: Correção na renderização do label do SegmentedButton

--- packages/ui-components/src/components/SegmentedButton/components/SegmentedButtonItem.tsx
+++ packages/ui-components/src/components/SegmentedButton/components/SegmentedButtonItem.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { styled, Button } from 'tamagui';
+import { styled, Button, View } from 'tamagui';
 import { LabelSmall } from '../../Typography';
 import { iconSize } from '../../../config/tokens/iconSize/iconSize';
-
 import { useTransformIcon } from '../../../utils';
 import { SegmentedButtonItemProps } from '../SegmentedButton.model';
 
@@ -12,10 +11,9 @@ const StyledButton = styled(Button, {
   flex: 1,
   height: 32,
   borderRadius: '$pill',
-  flexDirection: 'row',
-  alignItems: 'center',
+  paddingHorizontal: '$quark',
+  paddingVertical: '$quark',
   justifyContent: 'center',
-  padding: '$tiny',
 
   variants: {
     variant: {
@@ -103,12 +101,19 @@ export const SegmentedButtonItem = ({
           : 'transparent'
       }
     >
-      {btn.image ? (
-        <Image source={btn.image} width={20} height={20} />
-      ) : (
-        btn.icon && transformIcon(btn.icon, iconSize.small, iconColor)
-      )}
-      {btn.label && <LabelSmall color={textColor}>{btn.label}</LabelSmall>}
+      <View
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        gap="$nano"
+      >
+        {btn.image ? (
+          <Image source={btn.image} width={20} height={20} />
+        ) : (
+          btn.icon && transformIcon(btn.icon, iconSize.medium, iconColor)
+        )}
+        {btn.label && <LabelSmall color={textColor}>{btn.label}</LabelSmall>}
+      </View>
     </StyledButton>
   );
 };
